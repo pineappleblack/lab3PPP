77cb2fc5a4b33a17b7cf0f04d2f9f586d550d147
Alexey Smagin <asblackpn@gmail.com>
2016-04-01 07:52:48 +0300

    Add filework

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,26 @@
 package com.company;
 
+import java.io.*;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.println(new File(".").getAbsolutePath());
+	    try (FileInputStream fin = new FileInputStream("text.txt"))
+        {
+            int i=-1;
+            while (((i=fin.read())!=-1))
+            {
+                System.out.print((char)i);
+            }
+        }
+        catch (FileNotFoundException ex)
+        {
+            ex.printStackTrace();
+        }
+        catch (IOException ex)
+        {
+            ex.printStackTrace();
+        }
     }
 }
